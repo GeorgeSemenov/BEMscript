63cfa7d4a73406f34cf476ea3c0e1ffe0242478a
George Semenov <zhora.sem@yandex.ru>
2021-05-14 07:37:47 +0300

    zavis

--- BEMscripts/_findePugVariables.js
+++ BEMscripts/_findePugVariables.js
@@ -1,35 +1,60 @@
 const readFromFile = require('./readFromFile.js');
 const v = require('./variables.js');
 const func = function (fileName){
+  let variables = [];
   if (fileName == undefined){
     console.log(`-------------Error in findePugVariables module, fileName is undefined`);
     return -1;
   }else{
-    let dataFromFile = readFromFile(`${this.destination}/${fileName}/${fileName}.pug`)
-    let variableNameFirstSymbolPosition = 
-      dataFromFile.indexOf(v.SYMBOLS_BEFORE_NAME_OF_PUG_VARIABLE) + 
-      v.SYMBOLS_BEFORE_NAME_OF_PUG_VARIABLE.length;
+    let dataFromFile;
+    try{dataFromFile = readFromFile(`${this.destination}/${fileName}/${fileName}.pug`)}
+    catch(err){
+      console.log(`---------error in findePugVariable--------------\n${err}`);
+    }
+    let isNeedToContinue = false;
+    let variableNameFirstSymbolPosition = 0;
+    do{
+      isNeedToContinue = false;
+      variableNameFirstSymbolPosition = //Тут указанно, что нужно искать  первый символ имени переменной после предыдущего нахождения символа, чтобы функция не находила постоянно один и тот же символ.
+        dataFromFile.indexOf(
+          v.SYMBOLS_BEFORE_NAME_OF_PUG_VARIABLE,
+          variableNameFirstSymbolPosition) + 
+        v.SYMBOLS_BEFORE_NAME_OF_PUG_VARIABLE.length;
 
-    let variableNameLastSymbolPosition = 
-      dataFromFile.indexOf(v.SYMBOLS_AFTER_NAME_OF_PUG_VARIABLE,variableNameFirstSymbolPosition);
+      let variableNameLastSymbolPosition = 
+        dataFromFile.indexOf(v.SYMBOLS_AFTER_NAME_OF_PUG_VARIABLE,
+          variableNameFirstSymbolPosition);
 
-    let nameOfVariable = dataFromFile.slice(
-      variableNameFirstSymbolPosition,
-      variableNameLastSymbolPosition);
-    console.log(nameOfVariable);
+      let nameOfVariable = dataFromFile.slice(
+        variableNameFirstSymbolPosition,
+        variableNameLastSymbolPosition);
 
-    let variableDefValueFirstSymbolPosition = 
-      dataFromFile
-      .indexOf(v.SYMBOLS_BEFORE_DEFVALUE_OF_PUG_VARIABLE,variableNameFirstSymbolPosition)
-      + v.SYMBOLS_BEFORE_DEFVALUE_OF_PUG_VARIABLE.length;
+      let variableDefValueFirstSymbolPosition = 
+        dataFromFile
+        .indexOf(v.SYMBOLS_BEFORE_DEFVALUE_OF_PUG_VARIABLE,variableNameFirstSymbolPosition)
+        + v.SYMBOLS_BEFORE_DEFVALUE_OF_PUG_VARIABLE.length;
 
-    let variableDefValueLastSymbolPosition = dataFromFile.indexOf(
-      v.SYMBOLS_AFTER_DEFVALUE_OF_PUG_VARIABLE, variableDefValueFirstSymbolPosition);
+      let variableDefValueLastSymbolPosition = dataFromFile.indexOf(
+        v.SYMBOLS_AFTER_DEFVALUE_OF_PUG_VARIABLE, variableDefValueFirstSymbolPosition);
 
-    let defaultVariableValue = dataFromFile.
-      slice(variableDefValueFirstSymbolPosition,variableDefValueLastSymbolPosition)
-    console.log(defaultVariableValue);
+      let defaultVariableValue = dataFromFile.
+        slice(variableDefValueFirstSymbolPosition,variableDefValueLastSymbolPosition)
+      if (
+        (variableNameFirstSymbolPosition != -1) &&
+        (variableNameLastSymbolPosition != -1) &&
+        (variableDefValueFirstSymbolPosition != -1) &&
+        (variableDefValueLastSymbolPosition != -1)
+      ){
+        variableNameFirstSymbolPosition ++ ;
+        variables.push({
+          name : nameOfVariable,
+          defValue : defaultVariableValue
+        }) 
+        isNeedToContinue = true;
+      }
+    } while(isNeedToContinue)
   }
+  return variables;
 };
 
 module.exports = func;
