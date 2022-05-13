b8b01e9ed88318a36d92e52253c8d057e8314d31
GeorgeSemenov <zhora.sem@yandex.ru>
2022-05-13 10:10:43 +0300

    create function show.js but not complete it

--- BEMscripts/_show.js
+++ BEMscripts/_show.js
@@ -1,6 +1,43 @@
 
-const _show = function(cmd) {
+function enumirateAndShow(arr, ruleObj){
+  if (ruleObj == undefined){ 
+    arrayItemIndex = -1,
+    itemName = undefined,
+    isNeedToShowAllEmbedArrayItems = false,
+    embedRuleObj = {},
+    preEmbedSymbols=''
+  }
+  Пропиши свойства этого объекта по отдельности на случай, если они небудут определенны
+    
+}
 
+const _show = function(cmd) {
+  if(cmd){
+    if (cmd.toLowerCase() == "all"){
+      enumirateAndShow(this.blocks,{
+        isNeedToShowAllEmbedArrayItems : true,
+        preEmbedSymbols  :  '>',
+        embedRuleObj : {
+          isNeedToShowAllEmbedArrayItems : true,
+          preEmbedSymbols  :  '>>',
+          embedRuleObj : {
+            isNeedToShowAllEmbedArrayItems : false,
+            preEmbedSymbols  :  '>>>',
+          }
+        }
+      });
+    }else if(isFinite(cmd)){ // если команда - число
+      enumirateAndShow(thils.blocks,{
+        itemIndex: parseInt(cmd)
+      })
+    }else{
+      enumirateAndShow(thils.blocks,{
+        itemName: parseInt(cmd)
+      })
+    }
+  }else{
+    enumirateAndShow(thils.blocks)
+  }
 }
 
 module.exports = _show;
