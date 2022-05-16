24b79e12ab0aa4dfbfd79bb3367533201473649d
GeorgeSemenov <zhora.sem@yandex.ru>
2022-05-16 09:21:54 +0300

    finally end up with test show method[D[D[D[D[D[D[D[D[D[D[D_[2~show method

--- BEMscripts/tests/test_show.js
+++ BEMscripts/tests/test_show.js
@@ -15,7 +15,16 @@ bembd.show('all');
 // const readline = require('readline-sync');
 // let value = readline.question(`Enter value: `);//пример запроса ввода пользователем
 
+console.log(`bembd.show(0) ---------------------`);
+bembd.show(0);
 console.log(`bembd.show(2) ---------------------`);
 bembd.show('2');
 console.log(`bembd.show(1) ---------------------`);
 bembd.show(1);
+console.log(`bembd.show(Блок№1) ---------------------`);
+bembd.show(`Блок№1`);
+console.log(`bembd.show(invalid input) ---------------------`);
+bembd.show(`invalid input`);
+
+console.log(`bembd.show(pages) ---------------------`);
+bembd.show("pages");
