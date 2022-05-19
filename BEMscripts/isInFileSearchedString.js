bffcdae7c0faf788238e61a6520819b39116fd23
GeorgeSemenov <zhora.sem@yandex.ru>
2022-05-19 06:55:01 +0300

    begin to implement block scheme of Isin agj;lahg;f

--- BEMscripts/isInFileSearchedString.js
+++ BEMscripts/isInFileSearchedString.js
@@ -6,13 +6,12 @@ const readFromFile = require(`${__dirname}/readFromFile.js`);
 module.exports = function(fullFileName, searchedStr, isStrictMode = false){
   let fileContent = readFromFile(fullFileName);
   if (fileContent){//Проверяем, существует ли fullFileName, если не существует, то в fileContent будет false
-    if(isStrictMode){
-      let indexOfSearchedStr = fileContent.indexOf(searchedStr);
-      if(indexOfSearchedStr){//Если искомая строка не в начале файла (т.е. её индекс не равен нулю), то нужно проверить символ перед искомой строкой.
-
-      }
-    }else{return fileContent.includes(searchedStr)}//Если строгий режим оключён
-  }else{// Если fullFileName - не существует, то возращаем false
-    return false;
-  }
+    if(fileContent.includes(searchedStr)){
+      if(isStrictMode){
+        let indexOfSearchedStr = fileContent.indexOf(searchedStr);
+        if(indexOfSearchedStr){//Если искомая строка не в начале файла (т.е. её индекс не равен нулю), то нужно проверить символ перед искомой строкой.
+        }
+      }else{return true}//Если строгий режим выключен и при этом искомая строка содержится в файле, то возвращаем true
+    }else{return false}//Если искомая строка не найдена в указанном файле
+  }else{return false;}// Если fullFileName - не существует, то возращаем false
 }
