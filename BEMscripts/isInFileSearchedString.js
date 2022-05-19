7167c31db9d1f71c6e0bbd80797fb9ff60bb758b
GeorgeSemenov <zhora.sem@yandex.ru>
2022-05-19 11:26:03 +0300

    didn't test well isInFileSearchedString

--- BEMscripts/isInFileSearchedString.js
+++ BEMscripts/isInFileSearchedString.js
@@ -7,11 +7,30 @@ module.exports = function(fullFileName, searchedStr, isStrictMode = false){
   let fileContent = readFromFile(fullFileName);
   if (fileContent){//Проверяем, существует ли fullFileName, если не существует, то в fileContent будет false
     if(fileContent.includes(searchedStr)){
-      if(isStrictMode){
-        let indexOfSearchedStr = fileContent.indexOf(searchedStr);
-        if(indexOfSearchedStr){//Если искомая строка не в начале файла (т.е. её индекс не равен нулю), то нужно проверить символ перед искомой строкой.
-        }
+      if(isStrictMode){//Если включен строгий режим.
+        let indexOfSearchedStr = 0;
+        do{//Если искомая строка не в начале файла (т.е. её индекс не равен нулю), то нужно проверить символ перед искомой строкой.
+          indexOfSearchedStr = fileContent.indexOf(searchedStr,indexOfSearchedStr);
+          if(!(~indexOfSearchedStr)){return false}
+          else{//Если indexOfSearchedStr != -1 т.е. indexOf() - смог найти ещё одно совпадение
+            let symbolBfr = undefined;
+            let symbolAftr = undefined;
+            indexOfSearchedStr? symbolBfr = fileContent[indexOfSearchedStr - 1]: symbolBfr = undefined;//Если искомая строка не находится в начале файла(т.е. её индекс ≠ 0)
+            ((indexOfSearchedStr + searchedStr.length) < fileContent.length)? symbolAftr = fileContent[indexOfSearchedStr + searchedStr.length] : symbolAft = undefined;//Если искомая строка не находится в конце файла
+            // console.log(`symbolBfr = ${symbolBfr}\n symbolAftr = ${symbolAftr}`);
+            if(symbolBfr && symbolAftr){//Если оба символа существуют
+              if( (symbolBfr != '-') && (symbolAftr != '-') && (symbolBfr != '_') && (symbolAftr != '_') && (/[a-z]/i.test(symbolBfr) ) && (/[a-z]/i.test(symbolAftr)) ){return true;}//Если все условия выполняются Значит искомая строка с данными индексами и обрамляющими символами Не является частью другого слова и при этом равняется searchedStr)
+              else{indexOfSearchedStr++;}//Увеличиваем на единицу текущий индекс, чтобы следующая итерация цикла нашла другое значение indexOfSearchedStr
+            }else if(symbolBfr){//Если существет только символ перед искомой стр.
+              if((symbolBfr != '-') && (symbolBfr != '_') && (/[a-z]/i.test(symbolBfr)) ){return true}//Если все условия выполняются Значит искомая строка стоит в конце файлаи символ перед ней указывает на то что искомая строка не часть другого слова
+              else{indexOfSearchedStr++;}//Увеличиваем на единицу текущий индекс, чтобы следующая итерация цикла нашла другое значение indexOfSearchedStr
+            }else if(symbolAftr){//Если существет только символ после искомой стр
+              if((symbolAftr != '-') && (symbolAftr != '_') && (/[a-z]/i.test(symbolAftr))){return true}//Если существет только символ после искомой стр
+              else{indexOfSearchedStr++;}//Увеличиваем на единицу текущий индекс, чтобы следующая итерация цикла нашла другое значение indexOfSearchedStr
+            }else{return true}//Судя по всему раз нет символ до searchedStr и после, то в файле только searchedStr возвращаем true
+          }
+        }while(true)
       }else{return true}//Если строгий режим выключен и при этом искомая строка содержится в файле, то возвращаем true
-    }else{return false}//Если искомая строка не найдена в указанном файле
-  }else{return false;}// Если fullFileName - не существует, то возращаем false
+    }else{return false}//Если искомая строка не найдена в указанном файле то возвращаем false
+  }else{return false;}//Если fullFileName - не существует, то возращаем false
 }
--- BEMscripts/tests/test_isInFileSearchedString.js
+++ BEMscripts/tests/test_isInFileSearchedString.js
@@ -1,7 +1,14 @@
 const isInFileSearchedString = require(`${__dirname}/../isInFileSearchedString.js`);
 
-console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk.txt`,`прочтёт, тот`));//этот файл есть и в нём этот текст есть
-console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk23.txt`,`прочтёт, тот`));//этого файла нет
-console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk23.txt`,`прочтёт, тот ты`));//этого файла нет и этого текста в нём тоже нет
-console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk.txt`,`прочтёт, тот ты`));//этот файл есть, но в нём нет этого текста
-console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk.txt`,`Кто прочтёт`));//этот файл есть, но в нём нет этого текста
+// console.log(`Нестрогий поиск`);
+// console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk.txt`,`прочтёт, тот`));//этот файл есть и в нём этот текст есть(true)
+// console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk23.txt`,`прочтёт, тот`));//этого файла нет (false)
+// console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk23.txt`,`прочтёт, тот ты`));//этого файла нет и этого текста в нём тоже нет (false)
+// console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk.txt`,`прочтёт, тот ты`));//этот файл есть, но в нём нет этого текста (false)
+// console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/kekPuk.txt`,`Кто прочтёт`));//этот файл есть, и в нём есть этот текста (true)
+
+console.log(`\n\n\nСтрогий поиск`);
+console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/tag-block.pug`,`tag`,true));//этот файл есть и в нём этот текст есть, но он является частью другого слова(false)
+console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/tag-block.pug`,`tag-block__tag-name`,true));//этот файл есть и в нём этот текст есть, и он не является частью другого слова(true)
+console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/tag-block.pug`,`барабуль__tag-description`,true));//этот файл есть но в нём нет этого текста(false)
+console.log(isInFileSearchedString(`${__dirname}/testFOrMyOnlyWIshes/tag-block.pugввв`,`барабуль__tag-description`,true));//этого файла нет(false)
