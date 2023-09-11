bf20bb3c0bffb501e7bed35f492d7f4c3be40fa5
GeorgeSemenov <zhora.sem@yandex.ru>
2023-09-11 20:46:46 +0300

    add converter from mp4 to mp3

---
+++ BEMscripts/utilities/mp4_to_mp3/convert.ts
@@ -0,0 +1,18 @@
+//https://www.youtube.com/watch?v=z_CANxYAsfk
+const ffmpeg = require("fluent-ffmpeg");
+
+const fileName = "./video.mp4";
+const saveToFileName = "audio.mp3";
+
+ffmpeg(fileName)
+  .toFormat("mp3")
+  .saveToFile(saveToFileName, (stdout, stderr) => {})
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("progress", (pr) => {
+    console.log(`total file time = ${pr.timemark}`);
+  })
+  .on("end", () => {
+    console.log(`converting complete`);
+  });
