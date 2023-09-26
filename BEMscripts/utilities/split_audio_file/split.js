5dd4f364c950bf37dd716d002b24241368d17432
Семенов <@RIB.local>
2023-09-26 11:39:26 +0300

    concatenation is almost complete

--- BEMscripts/utilities/split_audio_file/split.js
+++ BEMscripts/utilities/split_audio_file/split.js
@@ -1,23 +1,34 @@
-//https://github.com/h2non/audioconcat#readme
-//Для работы этого скрипта в ubuntu нужно также уста ffmpeg
-//Для работы в windows нужно также установить ffmpeg с официального сайта(но это не точно, в убунту точно работает)
-const audioconcat = require("audioconcat");
-const ffmpeg = require("ffmpeg");
+//https://www.npmjs.com/package/mp3-cutter
+//npm i get-audio-duration
+//Для работы этого скрипта нужно установить mp3-cutter и get-audio-duration
 
-const arrLength = 24; //по умолчанию 5
-initialValue = 1; // по умолчанию 1
-let files = Array.from({ length: arrLength }, (_, i) => i + initialValue); //Если выставить значения по умолчанию, то получишь массив [1,2,3,4,5] т.е. набор цифр от initialValue до (arrLength + initialValue - 1) т.е. [1,2,3,4,5]
-files = files.map((item) => (item < 10 ? "0" : "") + item + ".mp3");
+const MP3Cutter = require("mp3-cutter");
+const { getAudioDurationInSeconds } = require("get-audio-duration");
 
-audioconcat(files)
-  .concat("audio.mp3")
-  .on("start", function (command) {
-    console.log("concatination process started:", command);
-  })
-  .on("error", function (err, stdout, stderr) {
-    console.error("Error:", err);
-    console.error("ffmpeg stderr:", stderr);
-  })
-  .on("end", function (output) {
-    console.error("Audio created in:", output);
-  });
+const originalFileName = "orig.mp3";
+let audioPartPrefixName = "segment";
+let audioPartPrefixNameExtension = "mp3";
+let start = 120; //В секундах
+const segmentDuration = 10; //в секундах
+
+let curTime;
+//duration в секундах
+getAudioDurationInSeconds(originalFileName).then((duration) => {
+  for (curTime = start; curTime < duration; curTime += segmentDuration) {
+    const est = curTime + segmentDuration; //end segment time
+    MP3Cutter.cut({
+      src: originalFileName,
+      target: `${audioPartPrefixName}(${ft(curTime)}__${ft(
+        est < duration ? est : duration
+      )}).${audioPartPrefixNameExtension}`,
+      start: curTime,
+      end: est < duration ? est : duration,
+    });
+  }
+});
+
+function ft(secs) {
+  return `${parseInt(secs / 3600)}-${parseInt((secs % 3600) / 60)}-${parseInt(
+    secs % 60
+  )}`;
+}
