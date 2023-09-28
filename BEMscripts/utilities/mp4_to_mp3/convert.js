137238edbf071bae9a786c19da2ca22e7737dcfd
GeorgeSemenov <zhora.sem@yandex.ru>
2023-09-28 06:17:45 +0300

    destructuring

--- BEMscripts/utilities/mp4_to_mp3/convert.js
+++ BEMscripts/utilities/mp4_to_mp3/convert.js
@@ -1,18 +1,22 @@
 //https://www.youtube.com/watch?v=z_CANxYAsfk
-const ffmpeg = require("fluent-ffmpeg");
+import ffmpeg from "fluent-ffmpeg";
 
 const fileName = "./video.mp4";
 const saveToFileName = "audio.mp3";
 
-ffmpeg(fileName)
-  .toFormat("mp3")
-  .saveToFile(saveToFileName, (stdout, stderr) => {})
-  .on("error", (err) => {
-    console.warn(err);
-  })
-  .on("progress", (pr) => {
-    console.log(`total file time = ${pr.timemark}`);
-  })
-  .on("end", () => {
-    console.log(`converting complete`);
-  });
+export default function convert(filteName, saveToFileName) {
+  ffmpeg(fileName)
+    .toFormat("mp3")
+    .saveToFile(saveToFileName, (stdout, stderr) => {})
+    .on("error", (err) => {
+      console.warn(err);
+    })
+    .on("progress", (pr) => {
+      console.log(`total file time = ${pr.timemark}`);
+    })
+    .on("end", () => {
+      console.log(`converting complete`);
+    });
+}
+
+convert(fileName, saveToFileName);
